4728e061e2ad508bb58ef1bcdb6dea8a4eced3af
Oleksii Korondevych <oleksii.korondevych@gmail.com>
2016-10-03 15:56:51 +0300

    set up denominations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+denom_list = [
+  [ "One", 1 ],
+  [ "Two", 2 ],
+  [ "Five", 5 ],
+  [ "Ten", 10 ],
+  [ "TwentyFive", 25 ],
+  [ "Fifty", 50 ]
+]
+
+denom_list.each do |type, denom|
+  Coin.create( type: type, denom: denom, amount: 0 )
+end
